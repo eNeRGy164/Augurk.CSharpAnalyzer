7edd3c574ba762099885baeafb8913e3e3229b27
Mark Taling <markt@infosupport.com>
2017-10-30 15:37:44 +0100

    Implemented first green test

--- Augurk.CSharpAnalyzer.Specifications/Features/Analyze.feature
+++ Augurk.CSharpAnalyzer.Specifications/Features/Analyze.feature
@@ -4,9 +4,9 @@ Scenario: Analyze
 Given 'Augurk.CSharpAnalyzer.Specifications' contains feature files
 And those features describe 'Augurk.CSharpAnalyzer'
 When an analysis is run
-Then the following report is returned for 'Augurk.CSharpAnalyzer'
-| Type           | Signature                                                                                  |
-| When           | an analysis is run                                                                         |
-| EntryPoint     | Augurk.CSharpAnalyzer.AnalyzeCommand.Execute(Augurk.CSharpAnalyzer.Options.AnalyzeOptions) |
-| Public-Invoke  | Oakton.ConsoleWriter.Write(ConsoleColor, string)                                           |
-| Private-Invoke | Augurk.CSharpAnalyzer.Analyze(Augurk.CSharpAnalyzer.Options.AnalyzeOptions)                |
+Then the first 1 lines of the reported return for 'Augurk.CSharpAnalyzer' are
+| Kind | Expression/Signature |
+| When | an analysis is run   |
+#| EntryPoint     | Augurk.CSharpAnalyzer.AnalyzeCommand.Execute(Augurk.CSharpAnalyzer.Options.AnalyzeOptions) |
+#| Public-Invoke  | Oakton.ConsoleWriter.Write(ConsoleColor, string)                                           |
+#| Private-Invoke | Augurk.CSharpAnalyzer.Analyze(Augurk.CSharpAnalyzer.Options.AnalyzeOptions)                |
--- Augurk.CSharpAnalyzer.Specifications/Steps/AnalyzerSteps.cs
+++ Augurk.CSharpAnalyzer.Specifications/Steps/AnalyzerSteps.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Augurk.CSharpAnalyzer.Collectors;
 using Augurk.CSharpAnalyzer.Commands;
 using Augurk.CSharpAnalyzer.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using TechTalk.SpecFlow;
 
 namespace Augurk.CSharpAnalyzer.Specifications.Steps
@@ -15,6 +18,7 @@ namespace Augurk.CSharpAnalyzer.Specifications.Steps
         private string _solution = Path.Combine(AppContext.BaseDirectory, @"..\..\..\Augurk.CSharpAnalyzer.sln");
         private string _targetProject;
         private string _sutProject;
+        private JToken _output;
 
         [Given(@"'(.*)' contains feature files")]
         public void GivenContainsFeatureFiles(string projectName)
@@ -43,26 +47,7 @@ namespace Augurk.CSharpAnalyzer.Specifications.Steps
             bool result = command.Execute(options);
             Assert.IsTrue(result, "An error occured during analysis.");
 
-            string resultingJson = collector.GetJsonOutput().ToString();
-        }
-
-        [When()]
-        [When(Culture="en-en", Regex=@"a third analysis is run")]
-        public void WhenAnotherAnalysisIsRun()
-        {
-            var options = new AnalyzeOptions();
-            options.Solution = _solution;
-            options.SpecificationsProject = _targetProject;
-            options.SystemUnderTest = _sutProject;
-
-            var command = new AnalyzeCommand();
-            var collector = new DefaultInvocationTreeCollector();
-            command.Collector = collector;
-
-            bool result = command.Execute(options);
-            Assert.IsTrue(result, "An error occured during analysis.");
-
-            string resultingJson = collector.GetJsonOutput().ToString();
+            _output = collector.GetJsonOutput();
         }
 
         [Then(@"the following report is returned for '(.*)'")]
@@ -72,9 +57,32 @@ namespace Augurk.CSharpAnalyzer.Specifications.Steps
         }
         
         [Then(@"the first (.*) lines of the reported return for '(.*)' are")]
-        public void ThenTheFirstLinesOfTheReportedReturnForAre(int numberOfCheckedLines, string projectName, Table table)
+        public void ThenTheFirstLinesOfTheReportedReturnForAre(int numberOfLinesToCheck, string projectName, Table table)
         {
-            ScenarioContext.Current.Pending();
+            if (_output == null)
+            {
+                Assert.Inconclusive("Please use a matching WHEN step before using this THEN step.");
+            }
+
+            Assert.AreEqual(numberOfLinesToCheck, table.RowCount, "The provided number of rows does not match the mentioned number of rows.");
+
+            List<dynamic> flatList = new List<dynamic>();
+            foreach (var invocation in _output)
+            {
+                JToken regExpressions = invocation["RegularExpressions"];
+
+                flatList.Add(new
+                {
+                    Kind = invocation["Kind"].Value<String>(),
+                    Signature = regExpressions == null ? invocation["Signature"].Value<String>() : String.Join(",", ((JArray)regExpressions).ToList())
+                });
+            }
+
+            for (int counter = 0; counter < numberOfLinesToCheck; counter++)
+            {
+                Assert.AreEqual(table.Rows[0]["Kind"], flatList[0].Kind, $"The Kind does not match on row {counter}");
+                Assert.AreEqual(table.Rows[0]["Expression/Signature"], flatList[0].Signature, $"The Expression/Signature does not match on row {counter}");
+            }
         }
     }
 }
