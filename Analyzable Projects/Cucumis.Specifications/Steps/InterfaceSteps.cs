07edde9d44f170db597fa29df2e47a02a7ebb787
Mark Taling <markt@infosupport.com>
2017-10-31 13:37:02 +0100

    Implemented some green scenarios for a change!

--- Analyzable Projects/Cucumis.Specifications/Steps/InterfaceSteps.cs
+++ Analyzable Projects/Cucumis.Specifications/Steps/InterfaceSteps.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Cucumis.Specifications.Support;
+﻿using Cucumis.Specifications.Support;
 using TechTalk.SpecFlow;
 
 namespace Cucumis.Specifications.Steps
---
+++ Analyzable Projects/Cucumis.Specifications/Support/InheritedGardener.cs
@@ -0,0 +1,20 @@
+﻿namespace Cucumis.Specifications.Support
+{
+    /// <summary>
+    /// A gardener who inherited it's job from it's parent. 
+    /// </summary>
+    /// <remarks>
+    /// Naturally, the child is more efficient.
+    /// </remarks>
+    class InheritedGardener : Gardener
+    {
+        /// <summary>
+        /// Plants a Gherkin AND waters it!
+        /// </summary>
+        internal void PlantGherkinAndWaterIt()
+        {
+            PlantGherkin();
+            WaterPlants();
+        }
+    }
+}
