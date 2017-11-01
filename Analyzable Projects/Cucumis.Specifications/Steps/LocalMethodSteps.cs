08f63b4e8aa9d42cfbc9be66d8e0fceb259f24e9
Mark Taling <markt@infosupport.com>
2017-11-01 08:59:37 +0100

    Added some additional scenarios

---
+++ Analyzable Projects/Cucumis.Specifications/Steps/LocalMethodSteps.cs
@@ -0,0 +1,25 @@
+﻿using Cucumis.Specifications.Support;
+using TechTalk.SpecFlow;
+
+namespace Cucumis.Specifications.Steps
+{
+    [Binding]
+    internal sealed class LocalMethodSteps
+    {
+        [When("a local method is called within the entrypoint")]
+        public void WhenALocalMethodIsCalledWithinTheEntrypoint()
+        {
+            Gherkin gherkin = new Gherkin();
+            // The OnWater will make a local call without the "this." identifier
+            gherkin.OnWater(new WaterEventArgs());
+        }
+
+        [When("an explicit base method is called within the entrypoint")]
+        public void WhenAnExplicitBaseMethodIsCalledWithinTheEntrypoint()
+        {
+            Gherkin gherkin = new PickyGherkin();
+            // The OnWater will make a base call with the "base." identifier
+            gherkin.OnWater(new WaterEventArgs());
+        }
+    }
+}
---
+++ Analyzable Projects/Cucumis/PickyGherkin.cs
@@ -0,0 +1,20 @@
+﻿namespace Cucumis
+{
+    /// <summary>
+    /// This class represents a Gherkin; the edible variety, not the written one.
+    /// </summary>
+    public class PickyGherkin : Gherkin
+    {
+        /// <summary>
+        /// This methods get called when this Gherkin is watered.
+        /// </summary>
+        public override void OnWater(WaterEventArgs args)
+        {
+            if (args.AcidFree)
+            {
+                // The explicit mention of base is relevant for the test!
+                base.Grow();
+            }
+        }
+    }
+}
